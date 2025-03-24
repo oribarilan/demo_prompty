0e6b2d4e133ae4383f220326ca748fbe8b41c3f6
ori <oribarilan@gmail.com>
2025-03-25 00:25:32 +0200

    updates

--- plzfile.py
+++ plzfile.py
@@ -1,7 +1,13 @@
-from pyplz import plz
+from pyplz import plz  # type: ignore
 
 
 @plz.task()
 def login():
     """AZ CLI login."""
     plz.run("az login --tenant 9bc6f80b-73d8-4d7c-ae26-572c276851a5")
+
+
+@plz.task()
+def run():
+    """Run the demo app"""
+    plz.run("python src/app/app.py")
